7a194c4351c281171bb60b02c66f8e0fb265389e
alanj853 <alanjackson.nuig@gmail.com>
2020-05-01 23:43:18 +0100

    start repo again

--- lib/your_weather.ex
+++ lib/your_weather.ex
@@ -9,7 +9,7 @@ defmodule YourWeather do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      # supervisor(YourWeather.Repo, []),
+      supervisor(YourWeather.Repo, []),
       # Start the endpoint when the application starts
       supervisor(YourWeather.Endpoint, []),
       # Start your own worker by calling: YourWeather.Worker.start_link(arg1, arg2, arg3)
