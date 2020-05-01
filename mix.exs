558d429227e32d125a1601ac2301950f46e1b704
alanj853 <alanjackson.nuig@gmail.com>
2020-05-01 23:42:57 +0100

    add json. Needed by absinthe plug

--- mix.exs
+++ mix.exs
@@ -42,8 +42,10 @@ defmodule YourWeather.Mixfile do
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:plug_cowboy, "~> 1.0"},
+      {:absinthe, "~> 1.4.16"},
       {:absinthe_ecto, "~> 0.1.3"},
       {:absinthe_plug, "~> 1.4.7"},
+      {:jason, "~> 1.2"}
     ]
   end
 
