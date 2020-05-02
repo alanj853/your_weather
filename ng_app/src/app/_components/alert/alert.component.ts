c0942e9ee366fdd903748a53afca036437f656af
alanj853 <alanjackson.nuig@gmail.com>
2020-05-03 00:43:40 +0100

    update alert

--- ng_app/src/app/_components/alert/alert.component.ts
+++ ng_app/src/app/_components/alert/alert.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AlertService } from '../../_services';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription;
+    message: any;
 
-  ngOnInit(): void {
-  }
+    constructor(private alertService: AlertService) { }
+
+    ngOnInit() {
+        this.subscription = this.alertService.getAlert()
+            .subscribe(message => {
+                switch (message && message.type) {
+                    case 'success':
+                        message.cssClass = 'alert alert-success';
+                        break;
+                    case 'error':
+                        message.cssClass = 'alert alert-danger';
+                        break;
+                }
+
+                this.message = message;
+            });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
 }
