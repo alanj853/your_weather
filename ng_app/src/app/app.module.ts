2b83d95409328e1cc74e22148a4bad11c2ee6b67
alanj853 <alanjackson.nuig@gmail.com>
2020-05-03 00:35:32 +0100

    update alert service and add alert component

--- ng_app/src/app/app.module.ts
+++ ng_app/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { fakeBackendProvider, JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AlertComponent } from './_components/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AlertComponent
   ],
   imports: [
     ReactiveFormsModule,
