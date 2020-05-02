a99178c7d72dc21aa408e04ef820d487ba49d077
alanj853 <alanjackson.nuig@gmail.com>
2020-05-03 00:32:35 +0100

    update fake backend and add alert service

--- ng_app/src/app/home/home.component.ts
+++ ng_app/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService, UserService } from '../_services';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  currentUser: any;
+  users = [];
 
-  ngOnInit(): void {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService
+  ) {
+      this.currentUser = this.authenticationService.currentUserValue;
+  }
+
+  ngOnInit() {
+      this.loadAllUsers();
+  }
+
+  deleteUser(id: number) {
+      this.userService.delete(id)
+          .pipe(first())
+          .subscribe(() => this.loadAllUsers());
+  }
+
+  private loadAllUsers() {
+      this.userService.getAll()
+          .pipe(first())
+          .subscribe(users => this.users = users);
   }
 
 }
