3a798d5310a68c0e6a5677208d0495895a9000c4
alanj853 <alanjackson.nuig@gmail.com>
2020-06-02 22:07:50 +0100

    return database data in user_resolver.ex

--- lib/your_weather/resolvers/user_resolver.ex
+++ lib/your_weather/resolvers/user_resolver.ex
@@ -1,6 +1,8 @@
 defmodule YourWeather.UserResolver do
+  alias YourWeather.Repo
+  alias YourWeather.User
   
     def all_users(_root, _args, _info) do
-      {:ok, []}
+      {:ok, Repo.all(User)}
     end
   end
