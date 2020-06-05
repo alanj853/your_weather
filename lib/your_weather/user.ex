6e1343d7a0e6aa145bedab4840ca62f02cb01401
alanj853 <alanjackson.nuig@gmail.com>
2020-06-05 15:51:33 +0100

    update user changeset

--- lib/your_weather/user.ex
+++ lib/your_weather/user.ex
@@ -9,6 +9,10 @@ defmodule YourWeather.User do
       field :last_name, :string
       field :password_hash, :string
       field :location, :string
+      
+      # Virtual fields:
+      field :password, :string, virtual: true
+      field :password_confirmation, :string, virtual: true
   
       timestamps()
     end
@@ -16,8 +20,23 @@ defmodule YourWeather.User do
     @doc false
     def changeset(user, attrs) do
       user
-      |> cast(attrs, [:email, :first_name, :last_name, :password_hash, :location])
-      |> validate_required([:email, :first_name, :last_name, :password_hash, :location])
+      |> cast(attrs, [:email, :first_name, :last_name, :password, :password_confirmation, :location]) # Remove hash, add pw + pw confirmation
+      |> validate_required([:email, :first_name, :last_name, :password, :password_confirmation, :location]) # Remove hash, add pw + pw confirmation
+      |> validate_format(:email, ~r/@/) # Check that email is valid
+      |> validate_length(:password, min: 8) # Check that password length is >= 8 
+      |> validate_confirmation(:password) # Check that password === password_confirmation
+      |> unique_constraint(:email)
+      |> put_password_hash()
+    end
+
+    defp put_password_hash(changeset) do
+      case changeset do
+        %Ecto.Changeset{valid?: true, changes: %{password: pass}}
+          ->
+            put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+        _ ->
+            changeset
+      end
     end
 
 end
